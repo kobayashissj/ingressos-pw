e604d41cf021161d07badbf1e1209ffd993cacf1
Dylan-Marinss <dylanmarins24@gmail.com>
2026-03-24 15:23:52 -0300

    Banner

---
+++ src/app/shared/components/em-cartaz/em-cartaz.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CardFilme } from "../card-filme/card-filme";
+import { MatButtonModule } from '@angular/material/button';
+
+
+
+@Component({
+  selector: 'app-em-cartaz',
+  imports: [CardFilme],
+  templateUrl: './em-cartaz.html',
+  styleUrl: './em-cartaz.css',
+})
+export class EmCartaz {
+
+}
